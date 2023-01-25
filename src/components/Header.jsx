7445c7cad1442ff96222ca84f93eef2888b28251
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-25 21:48:56 +0700

    create Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// import header logo
+import LogoWhite from "../assets/images/logo-white.svg";
+import LogoDark from "../assets/images/logo-dark.svg";
 
 const Header = () => {
+  const [header, setHeader] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+  });
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header
+      className={`${
+        header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
+      } fixed z-50 w-full transition-all duration-500 `}
+    >
+      <div className="container mx-auto flex flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0">
+        {/* header logo */}
+        <a href="/">
+          {header ? (
+            <img src={LogoDark} alt="header logo" className="w-[160px]" />
+          ) : (
+            <img src={LogoWhite} alt="header logo" className="w-[160px]" />
+          )}
+        </a>
+
+        {/* header menu */}
+        <div className={`${header ? "text-primary" : "text-white"}`}>
+          <ul className="flex items-center gap-x-6 font-tertiary text-[15px] uppercase tracking-[1.5px] lg:gap-x-8">
+            <a href="/" className="transition hover:text-accent">
+              Home
+            </a>
+            <a href="/" className="transition hover:text-accent">
+              Rooms
+            </a>
+            <a href="/" className="transition hover:text-accent">
+              Restaurants
+            </a>
+            <a href="/" className="transition hover:text-accent">
+              Spa
+            </a>
+            <a href="/" className="transition hover:text-accent">
+              Contact
+            </a>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 };
 
--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSlider = () => {
   return (
-    <div className="heroSlider h-[600px] bg-blue-200 lg:h-[860px]">
+    <div className="heroSlider h-[600px] bg-black lg:h-[860px]">
       <h1>HeroSlider</h1>
     </div>
   );
