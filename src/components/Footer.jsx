f08771cba502ff92aeef31af06e6e2613a97df9f
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-26 10:38:37 +0700

    create Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+// import logo
+import Logowhite from "../assets/images/logo-white.svg";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-y-6 text-white lg:flex-row lg:justify-between">
+        {/* footer logo */}
+        <a href="/">
+          <img src={Logowhite} alt="logo image" />
+        </a>
+        Copyrights &copy; 2023. All rights reseved.
+      </div>
+    </footer>
   );
 };
 
