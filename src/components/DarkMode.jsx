3daf2e35457825a544703c6ba8ea7b788bcb5ca3
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-27 10:46:08 +0700

    create: useLocalStorage & useColorMode hooks | add dark mode at Header component

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { BsMoonFill } from "react-icons/bs";
+import React, { useState } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+
+// import useColorMode hooks
+import useColorMode from "../hooks/useColorMode";
 
 const DarkMode = () => {
+  const [colorMode, setColorMode] = useColorMode();
+
   return (
-    <div className="fixed bottom-8 right-6 z-50 inline-flex cursor-pointer rounded-full bg-white p-3 text-xl text-accent shadow-[0_4px_16px_rgba(0,0,0,0.2)] lg:bottom-12 lg:right-10">
-      <BsMoonFill />
+    <div
+      className={`fixed bottom-8 right-6 z-50 inline-flex cursor-pointer rounded-full p-3 text-xl shadow-[0_4px_16px_rgba(0,0,0,0.2)] lg:bottom-12 lg:right-10 ${
+        colorMode === "light" ? "bg-white text-accent" : "bg-accent text-white"
+      }`}
+      onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}
+    >
+      {colorMode === "dark" ? <BsSunFill /> : <BsMoonFill />}
     </div>
   );
 };
